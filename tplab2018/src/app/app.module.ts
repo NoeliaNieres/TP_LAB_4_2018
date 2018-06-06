a94a8e97f898006abf26e13af3f6fff7d426fede
NoeliaNieres <macarenanieres@hotmail.com>
2018-06-06 18:46:49 -0300

    Estructura del proyecto

--- tplab2018/src/app/app.module.ts
+++ tplab2018/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    RegistroComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
